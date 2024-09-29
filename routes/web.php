373fba9daeac560b6fc162e155c37a39e5fbe6e5
abdulbasset-benz <benz.abdelbasset@gmail.com>
2024-09-29 22:10:54 +0100

    Add naming to routes

--- routes/web.php
+++ routes/web.php
@@ -9,12 +9,12 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/posts', [PostController::class, 'index']);
-Route::get('/posts/create', [PostController::class, 'create']);
-Route::post('/posts/store', [PostController::class, 'store']);
-Route::get('/posts/show', [PostController::class, 'show']);
-Route::put('/posts/{id}', [PostController::class, 'update']);
-Route::delete('/posts/{id}', [PostController::class, 'destroy']);
+Route::get('/posts', [PostController::class, 'index'])->name('posts.index');
+Route::get('/posts/create', [PostController::class, 'create'])->name('posts.create');
+Route::post('/posts/store', [PostController::class, 'store'])->name('posts.store');
+Route::get('/posts//{id}', [PostController::class, 'show'])->name('posts.show');
+Route::put('/posts/{id}', [PostController::class, 'update'])->name('posts.update');
+Route::delete('/posts/{id}', [PostController::class, 'destroy'])->name('posts.destroy');
 
 
 
