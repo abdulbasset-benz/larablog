a3e6f46fb3efe5e74d831f6d0ca57499dab0c2f9
abdulbasset-benz <benz.abdelbasset@gmail.com>
2024-10-01 20:59:15 +0100

    Create basic post submition form

--- resources/views/posts/create.blade.php
+++ resources/views/posts/create.blade.php
@@ -1,3 +1,20 @@
-<div>
-    <!-- Simplicity is an acquired taste. - Katharine Gerould -->
+ <!-- Simplicity is an acquired taste. - Katharine Gerould -->
+
+<div class="bg-slate-200 min-h-screen">
+    @vite('resources/css/app.css')
+    <div class="flex justify-center text-2xl pt-4 pb-4">
+        <h1>create a post with this form :</h1>
+    </div>
+    <div class=" max-w-2xl m-auto mt-3 ">
+        <form action="" method="post">
+            <div class="flex flex-col bg-slate-600">
+                <label for="title">Post title</label>
+                <input type="text" name="title" id="">
+                <label for="content">Your content goes here :</label>
+                <textarea name="content" id="" cols="30" rows="10">
+            </textarea>
+            </div>
+        </form>
+    </div>
+
 </div>
