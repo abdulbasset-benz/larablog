959fe7eb6e10a8785103b50f8da4a98612b75721
abdulbasset-benz <benz.abdelbasset@gmail.com>
2024-10-02 18:20:06 +0100

    Breeze post deletion commit

--- routes/web.php
+++ routes/web.php
@@ -18,14 +18,3 @@ Route::delete('/posts/{id}', [PostController::class, 'destroy'])->name('posts.de
 
 
 
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
-
-Route::middleware('auth')->group(function () {
-    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
-    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
-    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
-});
-
-require __DIR__.'/auth.php';
