872391ef8a9a77b088c887db80a91bb9797f01e1
abdulbasset-benz <benz.abdelbasset@gmail.com>
2024-10-02 10:28:09 +0100

    Modify fillable method

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -2,8 +2,9 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Support\Str;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Post extends Model
 {
@@ -12,7 +13,8 @@ class Post extends Model
     protected $fillable = [
         'title',
         'content',
-        'image'
+        'image',
+        'status'
     ];
 
     public function user()
