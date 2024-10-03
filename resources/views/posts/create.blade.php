56f824347394ff11e81d018514b18d4516cef0d4
abdulbasset-benz <benz.abdelbasset@gmail.com>
2024-10-03 12:33:28 +0100

    Add breeze for authentication

--- resources/views/posts/create.blade.php
+++ resources/views/posts/create.blade.php
@@ -1,5 +1,5 @@
 
-<div>
+<div class="max-w-2xl m-auto min-h-screen">
     @vite('resources/css/app.css')
     <h1 class=" bg-slate-600 ">this is an h1</h1>
 </div>
--- routes/web.php
+++ routes/web.php
@@ -1,20 +1,20 @@
 <?php
 
-use App\Http\Controllers\PostController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
-
 
 Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/posts', [PostController::class, 'index'])->name('posts.index');
-Route::get('/posts/create', [PostController::class, 'create'])->name('posts.create');
-Route::post('/posts/create', [PostController::class, 'store'])->name('posts.store');
-Route::get('/posts//{id}', [PostController::class, 'show'])->name('posts.show');
-Route::put('/posts/{id}', [PostController::class, 'update'])->name('posts.update');
-Route::delete('/posts/{id}', [PostController::class, 'destroy'])->name('posts.destroy');
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
 
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
 
-
+require __DIR__.'/auth.php';
