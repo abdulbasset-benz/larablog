bf51fd8dabd2cd11d4a40ac293b6e8eec240d3f6
abdulbasset-benz <benz.abdelbasset@gmail.com>
2024-10-04 16:17:45 +0100

    Edit post creating form

--- resources/views/posts/create.blade.php
+++ resources/views/posts/create.blade.php
@@ -1,5 +1,22 @@
+@vite('resources/css/app.css')
 
-<div class="max-w-2xl m-auto min-h-screen">
-    @vite('resources/css/app.css')
-    <h1 class=" bg-slate-600 ">this is an h1</h1>
+<div class=" bg-slate-300 max-w-2xl mx-auto mt-5 rounded-md p-5">
+    <h1>Create a porpusful Post :</h1>
+    <form action="{{ route('posts.store') }}" method="post" enctype="multipart/form-data"
+        class="flex flex-col mt-5 space-y-5 ">
+        @csrf
+
+        <label for="title">title</label>
+        <input type="text" name="title" id="">
+        <label for="content">Content</label>
+        <textarea name="content" id="" cols="30" rows="10" ></textarea>
+        <label for="image">image</label>
+        <input type="file" name="image" id="" class="bg-gray-200 py-3 px-4 rounded-md text-gray-800">
+        <div class="flex justify-center">
+            <button type="submit" class=" bg-blue-400 px-8 py-4 rounded-md uppercase  hover:bg-blue-500">submit
+                post</button>
+        </div>
+
+    </form>
+
 </div>
