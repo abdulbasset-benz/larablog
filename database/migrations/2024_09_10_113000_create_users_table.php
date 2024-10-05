f70cbe3d678e21cdc1ca1e2ef52b0f39bf9adf6e
abdulbasset-benz <benz.abdelbasset@gmail.com>
2024-10-05 20:26:31 +0100

    Modify the users migration

--- database/migrations/2024_09_10_113000_create_users_table.php
+++ database/migrations/2024_09_10_113000_create_users_table.php
@@ -12,7 +12,10 @@ class CreateUsersTable extends Migration
             $table->id();
             $table->string('name');
             $table->string('email')->unique();
+            $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->string('role')->default('directeur');
+            $table->rememberToken();
             $table->timestamps();
         });
     }
