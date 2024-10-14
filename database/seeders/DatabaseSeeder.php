1e84c304281bcf90483481ecbbd7101c3affb620
abdulbasset-benz <benz.abdelbasset@gmail.com>
2024-10-14 21:18:58 +0100

    Refactor database seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,9 +14,7 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // User::factory(10)->create();
-
-        User::factory(20)->create();
-        Post::factory(20)->create();
+        
+        
     }
 }
