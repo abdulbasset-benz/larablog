d67d29528d30cae683d16101f2e60a9b3e94c64b
abdulbasset-benz <benz.abdelbasset@gmail.com>
2024-10-14 21:14:28 +0100

    Refactor user migration

--- database/migrations/2024_09_10_113000_create_users_table.php
+++ database/migrations/2024_09_10_113000_create_users_table.php
@@ -14,7 +14,6 @@ class CreateUsersTable extends Migration
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
-            $table->string('role')->default('directeur');
             $table->rememberToken();
             $table->timestamps();
         });
