6fd7ce73c392c8216b0ead58aaf7ca0504df027f
abdulbasset-benz <benz.abdelbasset@gmail.com>
2024-10-28 22:07:58 +0100

    Delete images and Minor changes

--- resources/views/posts/create.blade.php
+++ resources/views/posts/create.blade.php
@@ -1,5 +1,7 @@
 @vite('resources/css/app.css')
 
+@extends('partials.navbar')
+
 <div class=" bg-slate-300 max-w-2xl mx-auto mt-5 rounded-md p-5">
     <h1>Create a porpusful Post :</h1>
     <form action="{{ route('posts.store') }}" method="post" enctype="multipart/form-data"
@@ -9,7 +11,7 @@
         <label for="title">title</label>
         <input type="text" name="title" id="">
         <label for="content">Content</label>
-        <textarea name="content" id="" cols="30" rows="10" ></textarea>
+        <textarea name="content" id="" cols="30" rows="10"></textarea>
         <label for="image">image</label>
         <input type="file" name="image" id="" class="bg-gray-200 py-3 px-4 rounded-md text-gray-800">
         <div class="flex justify-center">
