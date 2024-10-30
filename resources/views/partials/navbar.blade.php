e37e3fc2bc7940583ec12916d2e82f5440d9ca42
abdulbasset-benz <benz.abdelbasset@gmail.com>
2024-10-30 23:50:52 +0100

    Fix some issues with navbar

--- resources/views/partials/navbar.blade.php
+++ resources/views/partials/navbar.blade.php
@@ -8,7 +8,30 @@
 </head>
 
 <body>
-    
+    <header class="sticky top-0 w-full bg-white shadow-md">
+        <nav class="flex items-center justify-between max-w-6xl mx-auto p-4">
+            <div >
+                <a class="bg-red-600 py-5 px-10 rounded-md" href="{{ route('posts.index') }}">
+                    <span class="font-bold uppercase text-white">Lara-</span>
+                    <span class="font-bold uppercase text-green-400">DZ</span>
+                    
+                </a>
+            </div>
+            <div>
+                <ul class="flex gap-20 capitalize">
+                    <li><a class="hover:text-teal-600 font-semibold text-lg" href="{{ route('posts.index') }}">home</a></li>
+                    <li><a class="hover:text-teal-600 font-semibold text-lg" href="{{ route('posts.create')}}">Create a post</a></li>
+                    <li><a class="hover:text-teal-600 font-semibold text-lg" href="{{ route('posts.about')}}">about</a></li>
+                </ul>
+            </div>
+            
+            <div>
+                <button class="uppercase bg-teal-600 text-white font-bold px-8 py-3 rounded-md hover:bg-teal-500" type="button">log in</button>
+                <button class="uppercase text-black border-2 border-teal-600 font-bold px-8 py-2.5 rounded-md hover:text-white hover:bg-teal-600" type="button">sign up</button>      
+            </div>
+            
+        </nav>
+    </header>
 
 </body>
 
