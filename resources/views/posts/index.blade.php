2ae259c868c6fc51c875850966f7e20106caa5f2
abdulbasset-benz <benz.abdelbasset@gmail.com>
2024-10-30 23:49:57 +0100

    Build hero section of the blog

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -4,34 +4,37 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="preconnect" href="https://fonts.bunny.net">
+    <link href="https://fonts.bunny.net/css?family=aldrich:400|angkor:400" rel="stylesheet" />
     @vite('resources/css/app.css')
 </head>
 
 <body>
-    <header class="relative mb-24">
-        <nav class="flex justify-between items-center px-5 pt-5 fixed top-0 right-0 left-0 m-auto ">
-            <div >
-                <a class="bg-red-600 py-5 px-10 rounded-md" href="{{ route('posts.index') }}">
-                    <span class="font-bold uppercase text-white">Lara-</span>
-                    <span class="font-bold uppercase text-green-400">DZ</span>
-                    
-                </a>
+    @extends('layouts.app')
+
+    @section('content')
+        <main class="max-w-5xl h-screen mx-auto text-white">
+            <div class="flex flex-col justify-center items-center pt-5">
+                <img class=" w-96 " src="{{ asset('images/laravel-2.svg') }}" alt="">
+                <h1 class="text-[9rem] font-semibold font-aldrich text-slate-100">DZ</h1>
             </div>
-            <div>
-                <ul class="flex gap-20 capitalize">
-                    <li><a class="hover:text-teal-600 font-semibold text-lg" href="{{ route('posts.index') }}">home</a></li>
-                    <li><a class="hover:text-teal-600 font-semibold text-lg" href="{{ route('posts.create')}}">Create a post</a></li>
-                    <li><a class="hover:text-teal-600 font-semibold text-lg" href="{{ route('posts.about')}}">about</a></li>
-                </ul>
+            <h1 class="text-center text-2xl antialiased font-aldrich text-slate-200">Lara-DZ is where algerian laravel
+                developers
+                <div class="py-3">
+                    <span class="text-red-500 uppercase">gather,</span>
+                    <span class="text-white uppercase">share</span>
+                    <span class="text-green-500 uppercase">and contribute</span>
+                </div> to the algerian laravel community.
+            </h1>
+
+            <div class="text-center pt-5">
+                <button
+                    class="px-4 py-4 text-xl font-bold leading-6 capitalize duration-100 transform border-2 rounded-sm cursor-pointer border-green-500 focus:ring-4 focus:ring-green-500 focus:ring-opacity-50 focus:outline-none sm:w-auto sm:px-6 border-text  hover:shadow-lg hover:-translate-y-1 hover:bg-green-500">
+                    Explore More!</button>
             </div>
-            
-            <div>
-                <button class="uppercase bg-teal-600 text-white font-bold px-8 py-3 rounded-md hover:bg-teal-500" type="submit">log in</button>
-                <button class="uppercase text-black border-2 border-teal-600 font-bold px-8 py-2.5 rounded-md hover:text-white hover:bg-teal-600" type="submit">sign up</button>      
-            </div>
-            
-        </nav>
-    </header>
+        </main>
+    @endsection
+
 </body>
 
 </html>
