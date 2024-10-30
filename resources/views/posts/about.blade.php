cdc46c8bf1f504bcb83ad5a2048d814787254ba1
abdulbasset-benz <benz.abdelbasset@gmail.com>
2024-10-30 23:52:12 +0100

    Minor changes and images

--- resources/views/posts/about.blade.php
+++ resources/views/posts/about.blade.php
@@ -1,3 +1,8 @@
-<div>
-    <h1>about page</h1>
-</div>
+@extends('layouts.app')
+
+
+@section('content')
+    <main class="max-w-5xl h-screen mx-auto px-6 mt-6 text-white overflow-auto ">
+        <h1>hello from about page</h1>
+    </main>
+@endsection
--- resources/views/posts/create.blade.php
+++ resources/views/posts/create.blade.php
@@ -1,24 +1,30 @@
 @vite('resources/css/app.css')
 
-@extends('partials.navbar')
+@extends('layouts.app')
 
-<div class=" bg-slate-300 max-w-2xl mx-auto mt-5 rounded-md p-5">
-    <h1>Create a porpusful Post :</h1>
-    <form action="{{ route('posts.store') }}" method="post" enctype="multipart/form-data"
-        class="flex flex-col mt-5 space-y-5 ">
-        @csrf
 
-        <label for="title">title</label>
-        <input type="text" name="title" id="">
-        <label for="content">Content</label>
-        <textarea name="content" id="" cols="30" rows="10"></textarea>
-        <label for="image">image</label>
-        <input type="file" name="image" id="" class="bg-gray-200 py-3 px-4 rounded-md text-gray-800">
-        <div class="flex justify-center">
-            <button type="submit" class=" bg-blue-400 px-8 py-4 rounded-md uppercase  hover:bg-blue-500">submit
-                post</button>
+@section('content')
+    <main class="max-w-5xl h-screen mx-auto px-5 mt-7 text-white">
+        <div class=" bg-gray-600 max-w-2xl mx-auto mt-5 rounded-md p-5">
+            <h1>Create a porpusful Post :</h1>
+            <form action="{{ route('posts.store') }}" method="post" enctype="multipart/form-data"
+                class="flex flex-col mt-5 space-y-5 ">
+                @csrf
+
+                <label for="title">title</label>
+                <input type="text" name="title" id="">
+                <label for="content">Content</label>
+                <textarea name="content" id="" cols="30" rows="10"></textarea>
+                <label for="image">image</label>
+                <input type="file" name="image" id="" class="bg-gray-200 py-3 px-4 rounded-md text-gray-800">
+                <div class="flex justify-center">
+                    <button type="submit" class=" bg-blue-400 px-8 py-4 rounded-md uppercase  hover:bg-blue-500">submit
+                        post</button>
+                </div>
+
+            </form>
+
         </div>
 
-    </form>
-
-</div>
+    </main>
+@endsection
