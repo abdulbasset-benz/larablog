83c7ff58f58e5da7379f6a81a53a4c2783d9f49b
abdulbasset-benz <benz.abdelbasset@gmail.com>
2024-11-03 11:03:02 +0100

    Refactor code and adjust color

--- resources/views/posts/create.blade.php
+++ resources/views/posts/create.blade.php
@@ -4,17 +4,17 @@
 
 
 @section('content')
-    <main class="max-w-5xl h-screen mx-auto px-5 mt-7 text-white">
-        <div class=" bg-gray-600 max-w-2xl mx-auto mt-5 rounded-md p-5">
+    <main class="max-w-5xl h-screen mx-auto px-5 mt-7 text-slate-100">
+        <div class=" bg-gray-600 max-w-2xl mx-auto mt-5 rounded-md p-5 ">
             <h1>Create a porpusful Post :</h1>
             <form action="{{ route('posts.store') }}" method="post" enctype="multipart/form-data"
                 class="flex flex-col mt-5 space-y-5 ">
                 @csrf
 
                 <label for="title">title</label>
-                <input type="text" name="title" id="">
+                <input class="text-slate-900" type="text" name="title" id="">
                 <label for="content">Content</label>
-                <textarea name="content" id="" cols="30" rows="10"></textarea>
+                <textarea class="text-slate-900" name="content" id="" cols="30" rows="10"></textarea>
                 <label for="image">image</label>
                 <input type="file" name="image" id="" class="bg-gray-200 py-3 px-4 rounded-md text-gray-800">
                 <div class="flex justify-center">
