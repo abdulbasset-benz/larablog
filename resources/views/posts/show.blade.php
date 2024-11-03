4b31c3f5863d2d405bf3037aa32c9c8e02644a7e
abdulbasset-benz <benz.abdelbasset@gmail.com>
2024-11-03 22:08:49 +0100

    Create post page to display posts per Id

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -1,3 +1,10 @@
-<div>
-    <!-- Let all your things have their places; let each part of your business have its time. - Benjamin Franklin -->
+@vite('resources/css/app.css')
+@extends('layouts.app')
+@section('content')
+
+<div class="bg-gradient-to-r from-indigo-500 to-purple-600 p-12 rounded-lg shadow-lg flex flex-col items-center max-w-3xl mx-auto mt-10">
+    <h1 class="text-6xl font-extrabold text-white mb-6 text-center">{{ $post->title }}</h1>
+    <p class="text-lg text-gray-100 leading-relaxed text-center max-w-2xl">{{ $post->content }}</p>
 </div>
+
+@endsection
