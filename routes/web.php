3b4892c72d2e2916d6bbbaee9469421d8e2b6e22
abdulbasset-benz <benz.abdelbasset@gmail.com>
2024-11-04 19:00:01 +0100

    Minor changes

--- routes/web.php
+++ routes/web.php
@@ -4,7 +4,7 @@ use App\Http\Controllers\PostController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/welcome', function () {
+Route::get('/', function () {
     return view('welcome');
 });
 
